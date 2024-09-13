2e5f2bf28aa646025343f46bb70cbce147d7197b
Aelita <xsjctony@126.com>
2024-09-14 00:41:42 +1000

    wip: remark plugin

--- src/plugins/remarkMagicLink.ts
+++ src/plugins/remarkMagicLink.ts
@@ -3,8 +3,46 @@ import type { Root } from 'mdast'
 
 
 type ParsedMagicLink = {
-  text: string
-  link: string
+  text?: string
+  link?: string
+  type?: string
+  imageUrl?: string
+}
+
+type ResolvedMagicLink = Required<ParsedMagicLink>
+
+
+type MagicLinkHandler = {
+  name: string
+  handler: (content: string) => ParsedMagicLink | false | void
+  postprocess?: (resolved: ResolvedMagicLink) => ResolvedMagicLink | void
+}
+
+
+type LinksMapOption = {
+  linksMap: Record<string, string | { link: string; imageUrl?: string }>
+}
+
+type RemarkMagicLinkOptions = LinksMapOption & {
+  handlers?: MagicLinkHandler[]
+  imageOverrides?: [RegExp | string, string][]
+}
+
+
+function handlerLink(options: LinksMapOption): MagicLinkHandler {
+  const { linksMap } = options
+
+  return {
+    name: 'link',
+    handler(content: string) {
+      const type = 'link'
+      const text = content.trim()
+
+      let imageUrl: string | undefined
+
+      const linkDefaults = linksMap[text] ?? void 0
+    },
+  }
 }
 
 
