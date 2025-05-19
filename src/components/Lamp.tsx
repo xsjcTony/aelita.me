9b9f6d3c4ef7fc6b46f3e2ed78fb3f3ee8914884
Tony Jiang <tjiang@nowfinance.com.au>
2025-05-19 14:46:57 +1000

    style(lamp): add animation to aura

--- src/components/Lamp.tsx
+++ src/components/Lamp.tsx
@@ -1,3 +1,4 @@
+import type { Transition } from 'motion'
 import type { FC } from 'react'
 import { motion } from 'motion/react'
 import { cn } from '@utils/className.ts'
@@ -5,6 +6,13 @@ import { cn } from '@utils/className.ts'
 
 type LampProps = {
   className?: string
+}
+
+
+const TRANSITION: Transition = {
+  delay: 0.3,
+  duration: 0.8,
+  ease: 'easeInOut',
 }
 
 
@@ -19,27 +27,24 @@ const Lamp: FC<LampProps> = ({ className }) => (
     <motion.div
       className="absolute z-50 h-2 bg-gradient-to-r from-primary from-40% to-secondary to-60% w-[16rem] [--initial-width:8rem] [--expanded-width:16rem]"
       initial={{ width: 'var(--initial-width)' }}
-      transition={{
-        delay: 0.3,
-        duration: 0.8,
-        ease: 'easeInOut',
-      }}
+      transition={TRANSITION}
       whileInView={{ width: 'var(--expanded-width)' }}
     />
 
     {/* Aura */}
-    <div className="absolute z-50 blur-3xl h-144 w-[11rem] translate-y-1/4 rounded-full opacity-50 bg-gradient-to-r from-primary from-50% to-secondary to-50%" />
+    <motion.div
+      className="absolute z-50 blur-3xl h-144 translate-y-1/4 rounded-full opacity-50 bg-gradient-to-r from-primary from-50% to-secondary to-50% w-[12rem] [--initial-width:8rem] [--expanded-width:12rem]"
+      initial={{ width: 'var(--initial-width)' }}
+      transition={TRANSITION}
+      whileInView={{ width: 'var(--expanded-width)' }}
+    />
 
     {/* Light */}
     <div className="absolute z-30 w-full h-500 flex items-center justify-center mask-linear-from-50% mask-linear-to-50%">
       <motion.div
         className="blur-2xl h-144 translate-y-16 rounded-full bg-gradient-to-r from-primary from-50% to-secondary to-50% w-[7rem] [--initial-width:2rem] [--expanded-width:7rem]"
         initial={{ width: 'var(--initial-width)' }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: 'easeInOut',
-        }}
+        transition={TRANSITION}
         whileInView={{ width: 'var(--expanded-width)' }}
       />
     </div>
