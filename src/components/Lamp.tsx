9d2df7a6ab3a616ea71f8c4a29b01fd441526729
Tony Jiang <tjiang@nowfinance.com.au>
2025-05-23 14:57:43 +1000

    feat(lamp): only apply expand effect for once

--- src/components/Lamp.tsx
+++ src/components/Lamp.tsx
@@ -1,4 +1,4 @@
-import type { Transition } from 'motion'
+import type { Transition } from 'motion/react'
 import type { FC } from 'react'
 import { motion } from 'motion/react'
 import { cn } from '@utils/className.ts'
@@ -28,6 +28,7 @@ const Lamp: FC<LampProps> = ({ className }) => (
       className="absolute z-50 h-2 bg-gradient-to-r from-primary from-40% to-secondary to-60% w-[16rem] [--initial-width:8rem] [--expanded-width:16rem]"
       initial={{ width: 'var(--initial-width)' }}
       transition={TRANSITION}
+      viewport={{ once: true }}
       whileInView={{ width: 'var(--expanded-width)' }}
     />
 
@@ -36,6 +37,7 @@ const Lamp: FC<LampProps> = ({ className }) => (
       className="absolute z-50 blur-3xl h-144 translate-y-1/4 rounded-full opacity-50 bg-gradient-to-r from-primary from-50% to-secondary to-50% w-[12rem] [--initial-width:8rem] [--expanded-width:12rem]"
       initial={{ width: 'var(--initial-width)' }}
       transition={TRANSITION}
+      viewport={{ once: true }}
       whileInView={{ width: 'var(--expanded-width)' }}
     />
 
@@ -45,6 +47,7 @@ const Lamp: FC<LampProps> = ({ className }) => (
         className="blur-2xl h-144 translate-y-16 rounded-full bg-gradient-to-r from-primary from-50% to-secondary to-50% w-[7rem] [--initial-width:2rem] [--expanded-width:7rem]"
         initial={{ width: 'var(--initial-width)' }}
         transition={TRANSITION}
+        viewport={{ once: true }}
         whileInView={{ width: 'var(--expanded-width)' }}
       />
     </div>
