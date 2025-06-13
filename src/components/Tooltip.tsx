1c6c22ce6e5b41b329264164c2cfebdcc19d3ccb
Aelita <xsjctony@126.com>
2025-06-13 22:21:21 +1000

    style: improve tooltip shadow

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -35,7 +35,7 @@ const Tooltip: FC<TooltipProps> = ({
               <Content asChild sideOffset={10}>
                 <motion.div
                   animate={{ opacity: 1, y: 0, scale: 1 }}
-                  className="px-8 py-4 bg-bg rounded-lg shadow-sm shadow-(color:--shadow-color)"
+                  className="px-8 py-4 bg-bg rounded-lg shadow-sm drop-shadow-[0_0_0.4rem_color-mix(in_srgb,_var(--shadow-color)_40%,_transparent)]"
                   exit={{ opacity: 0, y: 10, scale: 0.8 }}
                   initial={{ opacity: 0, y: 10, scale: 0.8 }}
                   style={{ '--shadow-color': shadowColor }}
